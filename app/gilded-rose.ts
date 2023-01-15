b535fbdce655c46c9e98bd470e645a54e4532859
Zhi Hong <c6z3h@hotmail.com>
2023-01-15 13:19:19 +0800

    add new feature for Conjured items, delete eslint

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -23,27 +23,30 @@ export class GildedRose {
     // TODO Consider Functionl Programming -- but it seems overkill for this
     this.items.forEach((item) => {
       // 1. Sulfuras never needs update
+      if (item.name !== EItem.SULFURAS) item.sellIn -= 1
 
       // 2. Aged Brie update
       if (item.name === EItem.AGED_BRIE) {
         if (item.quality < 50) item.quality += 1;
-        item.sellIn -= 1;
         if (item.quality < 50 && item.sellIn < 0) item.quality += 1;
       }
 
       // 3. Backstage Pass update
       if (item.name === EItem.BACKSTAGE_PASS) {
         if (item.quality < 50) item.quality += 1;
-        if (item.sellIn < 11 && item.quality < 50) item.quality += 1;
-        if (item.sellIn < 6 && item.quality < 50) item.quality += 1;
-        item.sellIn -= 1;
+        if (item.sellIn < 10 && item.quality < 50) item.quality += 1;
+        if (item.sellIn < 5 && item.quality < 50) item.quality += 1;
         if (item.sellIn < 0) item.quality = 0;
       };
       
-      // 4. Other Items update
-      if ((item.name !== EItem.SULFURAS) && (item.name !== EItem.AGED_BRIE ) && (item.name !== EItem.BACKSTAGE_PASS)) {
+      // 4. Conjured items update
+      if (item.name === EItem.CONJURED_CAKE) {
+        if (item.quality > 0) item.quality = Math.max(item.quality - 2, 0)
+        if (item.quality > 0 && item.sellIn < 0) item.quality = Math.max(item.quality - 2, 0)
+      }
+      // 5. Other Items update
+      if ((item.name !== EItem.SULFURAS) && (item.name !== EItem.AGED_BRIE ) && (item.name !== EItem.BACKSTAGE_PASS) && (item.name !== EItem.CONJURED_CAKE)) {
         if (item.quality > 0) item.quality -= 1;
-        item.sellIn -= 1;
         if (item.quality > 0 && item.sellIn < 0) item.quality -= 1;
       }
     })
--- test/golden-master-text-test.ts
+++ test/golden-master-text-test.ts
@@ -10,7 +10,8 @@ export const items = [
   new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
   new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
   // this conjured item does not work properly yet
-  // new Item("Conjured Mana Cake", 3, 6)
+  new Item("Conjured Mana Cake", 3, 6),
+  new Item("Conjured Mana Cake", 3, 20)
 ];
 
 
@@ -22,12 +23,12 @@ if (process.argv.length > 2) {
   }
 
 for (let i = 0; i < days; i++) {
-  // console.log("-------- day " + i + " --------");
-  // console.log("name, sellIn, quality");
-  // items.forEach(element => {
-  //   console.log(element.name + ' ' + element.sellIn + ' ' + element.quality);
+  console.log("-------- day " + i + " --------");
+  console.log("name, sellIn, quality");
+  items.forEach(element => {
+    console.log(element.name + ' ' + element.sellIn + ' ' + element.quality);
 
-  // });
-  // console.log();
+  });
+  console.log();
   gildedRose.updateQuality();
 }
